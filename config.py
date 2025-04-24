242c06233a5396f923c6777d28ea5ad564218c78
skvsmdarman <136343252+skvsmdarman@users.noreply.github.com>
2025-04-24 13:18:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "sample_mflix")
 # 
 
 SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "api.modijiurl.com/api")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "086c301fc06b961106da0c905cde06c368207ba0"")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "086c301fc06b961106da0c905cde06c368207ba0")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
 TUT_VID = os.environ.get("TUT_VID", "https://t.me/+HomcJslE1n03YjBl") # shareus ka tut_vid he 
